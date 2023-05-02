3dd610b83dc229f269026904e628ff8e92dad319
Josh Bleecher Snyder <josharian@gmail.com>
2023-05-02 14:16:41 -0700

    internal/fmtsort: upgrade to Go 1.20.4

--- internal/fmtsort/sort_test.go
+++ internal/fmtsort/sort_test.go
@@ -148,7 +148,7 @@ func sprint(data any) string {
 		}
 		b.WriteString(sprintKey(key))
 		b.WriteRune(':')
-		b.WriteString(fmt.Sprint(om.Value[i]))
+		fmt.Fprint(b, om.Value[i])
 	}
 	return b.String()
 }
