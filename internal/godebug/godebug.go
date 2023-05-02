006a2a4de8054fd44449350503f7201f6f8d2c78
Josh Bleecher Snyder <josharian@gmail.com>
2023-05-02 14:37:09 -0700

    internal/godebug: add stub package to mimic Go 1.20.4

---
+++ internal/godebug/godebug.go
@@ -0,0 +1,32 @@
+package godebug
+
+// A Setting is a single setting in the $GODEBUG environment variable.
+type Setting struct {
+	name string
+}
+
+// New returns a new Setting for the $GODEBUG setting with the given name.
+func New(name string) *Setting {
+	return &Setting{name: name}
+}
+
+// Name returns the name of the setting.
+func (s *Setting) Name() string {
+	return s.name
+}
+
+// String returns a printable form for the setting: name=value.
+func (s *Setting) String() string {
+	return s.name + "=" + s.Value()
+}
+
+// Value returns the current value for the GODEBUG setting s.
+//
+// Value maintains an internal cache that is synchronized
+// with changes to the $GODEBUG environment variable,
+// making Value efficient to call as frequently as needed.
+// Clients should therefore typically not attempt their own
+// caching of Value's result.
+func (s *Setting) Value() string {
+	return ""
+}
